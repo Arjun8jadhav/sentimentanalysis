3ea2461838c7a553785f381306794f205b775092
Arjun8jadhav <121244507+Arjun8jadhav@users.noreply.github.com>
2022-12-23 11:03:09 +0530

    Update sentimentanalysis.py

--- sentimentanalysis.py
+++ sentimentanalysis.py
@@ -1,6 +1,7 @@
 #A__R__J__U__N___J__A__D__H__A__V
 import requests
 import json
+from time import sleep
 from textblob import TextBlob
 newapi=requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=8cf65409ff014a3abedd9dce16e00925')
 #print(newapi.content) we have to convert it to json make more readful
@@ -20,4 +21,5 @@ print(sentimentrank)
 print(data['articles'][n]['content'])
 print("read full article: ")
 print(data['articles'][n]['url'])
+sleep(3)
 
